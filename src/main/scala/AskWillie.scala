891215534d10978ade2461ebd28b0fddb3688c11
dytryk <astonmartinone111111@gmail.com>
2024-04-10 20:58:48 -0400

    working

--- src/main/scala/AskWillie.scala
+++ src/main/scala/AskWillie.scala
@@ -16,7 +16,12 @@ import scala.util.Sorting
         val pages: Map[String, WebPage] = mapWebPages(loadWebPages()) // completed for you
 
         // TODO: Measure the importance of each page using one of the functions in PageRank
-        val rankedPages: List[RankedWebPage] = List() // call PageRank.???? here
+//        val rankedPages: List[RankedWebPage] = List() // call PageRank.???? here
+//        val rankedPages: List[RankedWebPage] = PageRank.equal(pages).toList.map{case (page, weight) => RankedWebPage(pages(page), weight)} // call PageRank.???? here
+        val rankedPages: List[RankedWebPage] = PageRank.equal(mapWebPages(loadWebPages())).toList.map {
+          case (page, weight) => RankedWebPage(pages(page), weight)
+        }
+
 
         // Get user input then perform search until ":quit" is entered
         var query: String = ""
@@ -30,12 +35,34 @@ import scala.util.Sorting
             terms != List(":quit")
         } do {
           // TODO: Measure the textual match of each page to these terms using one of the functions in PageSearch
-          val searchedPages: List[SearchedWebPage] = List() // call PageSearch.???? here
+//          val searchedPages: List[SearchedWebPage] = List() // call PageSearch.???? here
+//          val searchedPages: List[SearchedWebPage] = PageSearch.count(rankedPages.map(_.page), case (page, textMatch) => SearchedWebPage(page, textMatch)) // call PageSearch.???? here
+//          val searchedPages: List[SearchedWebPage] = PageSearch.count(rankedPages.map(_.page)).map {case (page, textMatch) => SearchedWebPage(page, textMatch)}
+//          val searchedPages: List[SearchedWebPage] = PageSearch.count(rankedPages, terms).map{case(webPage, weight, textMatch) => SearchedWebPage(rankedPages(webPage, weight), textMatch)}
+//          val searchedPages: List[SearchedWebPage] = PageSearch.count(rankedPages, terms).map{case(webPage, weight, textMatch) => new SearchedWebPage(webPage, weight, textMatch)}
+//          val searchedPages: List[SearchedWebPage] = pages.map { rankedPage => val textMatch = PageSearch.count(rankedPages, terms).head new SearchedWebPage(rankedPage, textMatch)}
+//          val searchedPages: List[SearchedWebPage] = pages.map { rankedPage =>
+//            val textMatch = PageSearch.count(rankedPages, terms).head
+//            new SearchedWebPage(rankedPage, textMatch)
+//          }
+          // Inside the search loop
+//          val searchedPages: List[SearchedWebPage] = mapWebPages(loadWebPages()).values.map { rankedPage =>
+//            val textMatch = PageSearch.count(List(rankedPage), terms).head
+//            new SearchedWebPage(rankedPage, textMatch)
+//          }.toList
+          // Inside the search loop
+          val searchedPages: List[SearchedWebPage] = mapWebPages(loadWebPages()).values.map { webPage =>
+            val rankedPage = rankedPages.find(_.id == webPage.id).getOrElse(throw new NoSuchElementException(s"No ranked page found for ${webPage.id}"))
+            val textMatch = PageSearch.count(List(rankedPage), terms).head
+            new SearchedWebPage(rankedPage, textMatch)
+          }.toList
+
+
           // normalize the ranges for weight and textmatch on these pages
           val pageArray = SearchedWebPageNormalize.normalize(searchedPages).toArray
           // sort this array based on the chosen averaging scheme i.e.
           //    (ArithmeticOrdering || GeometricOrdering || HarmonicOrdering)
-          Sorting.quickSort(pageArray)(NameOrdering) // TODO: change this from name ordering to something else!!!
+          Sorting.quickSort(pageArray)(ArithmeticOrdering) // TODO: change this from name ordering to something else!!!
           // Print the top ranked pages in descending order
           for p <- pageArray.reverse.slice(0, 10) do println(f"${p.name}%-15s  ${p.url}")
           // print a divider to make reading the results easier
--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -7,7 +7,6 @@ object PageRank {
      * @return      A map of page.id to a weight of 1.0 for those same WebPage objects
      */
     def equal(pages: Map[String, WebPage]): Map[String, Double] = {
-        Map() // TODO: remove this stub and implement this method
         pages.map{case (k, v) => k -> 1.0}
     }
 
